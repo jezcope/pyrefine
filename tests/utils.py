5ec21d5a15600dc3a6c3d8176d9d82679e9a584c
Jez Cope <j.cope@erambler.co.uk>
2017-05-15 07:27:10 +0100

    Split test_pyrefine.py up

---
+++ tests/utils.py
@@ -0,0 +1,15 @@
+def print_frame_differing_rows(df1, df2):
+    """Print the difference between two :class:`DataFrame`s
+
+    Prints the rows of the two DataFrames which differ, along with a
+    boolean DataFrame indicating which individual values differ.
+
+    Args:
+        df1, df2 (:class:`pandas.DataFrame`): The two DataFrames to
+            compare.
+    """
+    diff = (df1 == df2) | df1.isnull() | df2.isnull()
+    rows = ~(diff.all(axis=1))
+    print(df1[rows])
+    print(df2[rows])
+    print(diff[rows])
